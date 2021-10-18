d61be5bc4392b4a24b0c18a48e1bb3a710fc4cd6
Anton <anton@smallfeet.de>
2021-10-18 12:27:33 +0200

    Added main method

--- src/Mitarbeiter.java
+++ src/Mitarbeiter.java
@@ -28,5 +28,18 @@ public abstract class Mitarbeiter {
     public abstract float entgeldBerechnen();
 
     public static void main(String[] args) {
+        Mitarbeiter zeitarbeiter = new Zeitarbeiter("Name", "Name", 21.50);
+        Mitarbeiter angestellter = new Angestellter("Name", "Name", 2134.20, 25);
+        Mitarbeiter atAngestellter = new ATAngestellter("Name", "Name", 2641.78);
+
+        System.out.println(zeitarbeiter.toString() + ":"
+                + "\nMonatsgehalt: " + zeitarbeiter.entgeldBerechnen()
+                + "\nJahresgehalt bis Heute: " + zeitarbeiter.getJahresGehaltBisHeute());
+        System.out.println(angestellter.toString() + ":"
+                + "\nMonatsgehalt: " + angestellter.entgeldBerechnen()
+                + "\nJahresgehalt bis Heute: " + angestellter.getJahresGehaltBisHeute());
+        System.out.println(atAngestellter.toString() + ":"
+                + "\nMonatsgehalt: " + atAngestellter.entgeldBerechnen()
+                + "\nJahresgehalt bis Heute: " + atAngestellter.getJahresGehaltBisHeute());
     }
 }
