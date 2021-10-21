27ce436f58f60520706c28b6d247e45ae0b7c6e1
Wassimasri <92931285+Wassimasri@users.noreply.github.com>
2021-10-21 15:42:22 +0200

    Update ATAngestellter.java

--- src/ATAngestellter.java
+++ src/ATAngestellter.java
@@ -4,17 +4,31 @@ import java.time.LocalDateTime;
  *
  */
 
-class ATAngestellter extends Mitarbeiter {
+class ATAngestellter  implements IMitarbeiter, ISteuerzahler
+{
+    private vertragsArtT vertrag = vertragsArtT.ANGESTELLTER;
+    private String vorname;
+    private String nachname;
     private float monatsLohn;
 
-    public ATAngestellter(String vorname, String nachname, float monatsLohn) {
-        super(vorname, nachname);
-        this.monatsLohn = monatsLohn;
-    }
+    public ATAngestellter(String vorname, String nachname, float monatsLohn)
+    {
+        if (monatsLohn/160 < mindestLohn){
+            System.err.println("Der ATAngestellte " + vorname + " " + nachname + " erhält nicht den gesetzlich festgesetzten Mindestlohn!");
+            }
+            this.vorname = vorname;
+            this.nachname = nachname;
+            this.monatsLohn = monatsLohn;
 
     public float getmonatsLohn() {
         return monatsLohn;
     }
+     
+     public float getJahresGehaltBisHeute() {
+            return jahresGehaltBisHeute;
+        }
+        
+        private vertragsArtT vertrag = vertragsArtT.ATANGESTELLTER
 
     @Override
     public float entgeltBerechnen() {
@@ -24,4 +38,16 @@ class ATAngestellter extends Mitarbeiter {
         jahresGehaltBisHeute += monatsLohn;
         return monatsLohn;
     }
+    
+     public float tatsächlicheEinkommenSteuer() {
+            return jahresGehaltBisHeute * 0.36f;
+        }
+
+        @Override
+        public float voraussichtlicheEinkommenSteuer() {
+            float durchschnittsGehalt =
+                    jahresGehaltBisHeute * 12
+                    / LocalDateTime.now().getMonthValue();
+            return durchschnittsGehalt * 0.36f;
+        }
 }
