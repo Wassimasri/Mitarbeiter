7d935f80cdde56d8432c7a7ff4f946452e3f7963
ntn98 <63919788+ntn98@users.noreply.github.com>
2021-10-21 15:50:51 +0200

    Update ATAngestellter.java
    added jahresGehaltBisHeute and toString()

--- src/ATAngestellter.java
+++ src/ATAngestellter.java
@@ -1,27 +1,55 @@
 import java.time.LocalDateTime;
 
 /** @author Wassim Almasri
- *
+*
  */
 
-class ATAngestellter extends Mitarbeiter {
+class ATAngestellter  implements IMitarbeiter, ISteuerzahler
+{
+    private vertragsArtT vertrag = vertragsArtT.ANGESTELLTER;
+    private String vorname;
+    private String nachname;
     private float monatsLohn;
+    private float jahresGehaltBisHeute;
 
-    public ATAngestellter(String vorname, String nachname, float monatsLohn) {
-        super(vorname, nachname);
+    public ATAngestellter(String vorname, String nachname, float monatsLohn)
+    {
+        if (monatsLohn/160 < mindestLohn){
+            System.err.println("Der ATAngestellte " + vorname + " " + nachname + " erhält nicht den gesetzlich festgesetzten Mindestlohn!");
+        }
+        this.vorname = vorname;
+        this.nachname = nachname;
         this.monatsLohn = monatsLohn;
-    }
 
     public float getmonatsLohn() {
         return monatsLohn;
     }
 
+    public float getJahresGehaltBisHeute() {
+       return jahresGehaltBisHeute;
+    }
+    
+    @Override
+    public String toString() {
+       return vertrag + ": " + nachname + ", " + vorname;
+    }
+
     @Override
     public float entgeltBerechnen() {
-        if (LocalDateTime.now().getMonthValue() == 1){
-            jahresGehaltBisHeute = 0;
-        }
         jahresGehaltBisHeute += monatsLohn;
         return monatsLohn;
     }
+    
+    @Override
+    public float tatsächlicheEinkommenSteuer() {
+        return jahresGehaltBisHeute * 0.36f;
+    }
+
+     @Override
+     public float voraussichtlicheEinkommenSteuer() {
+        float durchschnittsGehalt =
+                jahresGehaltBisHeute * 12
+                / LocalDateTime.now().getMonthValue();
+        return durchschnittsGehalt * 0.36f;
+     }
 }
